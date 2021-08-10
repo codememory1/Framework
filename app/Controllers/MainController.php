412f51c7e20fe4ae0074aaa03f7f01a1650635ce
Danil <kostynd1@gmail.com>
2021-08-11 00:34:21 +0300

    Изменно: конфигурация. Добавленно: маршрут и шаблон по умолчанию

---
+++ app/Controllers/MainController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controllers;
+
+use Kernel\AbstractController;
+
+/**
+ * Class MainController
+ *
+ * @package App\Controllers
+ *
+ * @author  Codememory
+ */
+class MainController extends AbstractController
+{
+
+    /**
+     * @return void
+     */
+    public function main(): void
+    {
+
+        $this->render('codememory', [
+            'controller' => __CLASS__,
+            'method'     => __METHOD__
+        ]);
+
+    }
+
+}
